eb31a30f199d9a0b1e162048adb75e40d4b805ee
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2023-01-12 10:24:33 -0300

    Respondendo a pergunta

--- model/questao.ts
+++ model/questao.ts
@@ -40,7 +40,15 @@ export default class QuestaoModel {
         return false
     }
 
-    
+    responderCom(indice: number): QuestaoModel {
+        const acertou = this.#respostas[indice]?.certa
+        const respostas = this.#respostas.map((resposta, i) => {
+            const respostaSelecionada = indice === i
+            const deveRevelar = respostaSelecionada || resposta.certa
+            return deveRevelar ? resposta.revelar() : resposta
+        })
+    return new QuestaoModel(this.id, this.enunciado, respostas, acertou)
+    }
 
     embaralharRespostas(): QuestaoModel {
         let respostasEmbaralhadas = embaralhar(this.#respostas)
@@ -52,6 +60,7 @@ export default class QuestaoModel {
             id: this.#id,
             enunciado: this.#enunciado,
             respostas: this.#respostas.map(resp => resp.paraObjeto()),
+            respondida: this.respondida,
             acertou: this.#acertou
         }
     }
