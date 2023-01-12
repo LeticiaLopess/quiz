44360d93983e75f458059b0ca9d40e27fc352bcd
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2023-01-11 23:09:56 -0300

    construindo a API

--- pages/api/questoes/[id].ts
+++ pages/api/questoes/[id].ts
@@ -1,5 +1,14 @@
 import questoes from "../bancoDeQuestoes"
 
 export default (req: any, res: any) => {
-    res.status(200).json(questoes[0].paraObjeto())
+    const idSelecionado = Number(req.query.id)
+
+    const unicaQuestaoOuNada = questoes.filter(questao => questao.id === idSelecionado)
+
+    if (unicaQuestaoOuNada.length === 1) {
+        const questaoSelecionada = unicaQuestaoOuNada[0]
+        res.status(200).json(questaoSelecionada.paraObjeto())
+    } else {
+        res.status(204).send()
+    }
 }
