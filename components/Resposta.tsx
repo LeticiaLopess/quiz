db2023a3e4ca9653011a9f5cf342069fd9b96f79
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2023-01-14 17:53:14 -0300

    revelação da resposta

--- components/Resposta.tsx
+++ components/Resposta.tsx
@@ -15,7 +15,8 @@ export default function Resposta(props: RespostaProps) {
     return (
         <div className={styles.resposta} onClick={() => props.respostaFornecida(props.indice)}>
             <div className={styles.conteudoResposta}>
-                <div className={styles.frente}>
+                {!resposta.revelada ? (
+                    <div className={styles.frente}>
                     <div className={styles.letra} style={{ backgroundColor: props.corFundoLetra}}>
                         {props.letra}
                     </div>
@@ -23,9 +24,21 @@ export default function Resposta(props: RespostaProps) {
                         {resposta.valor}
                     </div>
                 </div>
-                <div className={styles.verso}>
-
+                ) : (
+                    <div className={styles.verso}>
+                    {resposta.certa ? (
+                    <div className={styles.certa}>
+                        <div>A resposta certa é...</div>
+                        <div className={styles.valor}>{resposta.valor}</div>
+                    </div>
+                    ) : (
+                    <div className={styles.errada}>
+                        <div>A resposta informada está errada...</div>
+                        <div className={styles.valor}>{resposta.valor}</div>
+                    </div>
+                    )}
                 </div>
+                )}
             </div>
         </div>
     )
