6db2d949ab292751e76c79938d504035b200d2ab
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2023-01-13 22:48:08 -0300

    css opções de resposta

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import RespostaModel from '../model/resposta'
 
 
 export default function Home() {
-  const questaoTeste = new QuestaoModel(1, 'Melhor cor?', [
+  const questaoTeste = new QuestaoModel(1, 'Qual é a melhor cor?', [
     RespostaModel.errada('Verde'),
     RespostaModel.errada('Vermelha'),
     RespostaModel.errada('Azul'),
