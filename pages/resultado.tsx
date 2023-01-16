81ed187f6dd090b495b3ed52787265554a1ff1f5
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2023-01-15 22:44:55 -0300

    componente resultado

--- pages/resultado.tsx
+++ pages/resultado.tsx
@@ -1,7 +1,19 @@
+import styles from '../styles/Resultado.module.css'
+import { useRouter } from 'next/router'
+
 export default function resultado() {
+    const router = useRouter ()
+
+    const total = Number(router.query.total)
+    const certas = Number(router.query.certas)
+    const percentual = Math.round((certas/total * 100))
+
     return (
-        <div>
-            <h1>Resultado</h1>
+        <div className={styles.resultado}>
+            <h1>Resultado Final</h1>
+            <div>{total}</div>
+            <div>{certas}</div>
+            <div>{`${percentual}%`}</div>
         </div>
     )
 }
