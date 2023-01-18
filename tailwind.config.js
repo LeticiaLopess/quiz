084724f70031a0139bab553637b808d95f80b2a0
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2023-01-18 10:02:36 -0300

    att

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  darkMode: false,
   theme: {
     extend: {},
+  },
+  variants: {
+    extended: {},
   },
   plugins: [],
 }
