b66fc06f36148c65275c6956a8c113fef784666f
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2023-01-19 18:32:58 -0300

    att

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Questionario from '../components/Questionario'
 import QuestaoModel from '../model/questao'
 import { useRouter } from 'next/router'
 
-const BASE_URL = 'https://https://quiz-zeta-seven.vercel.app//api'
+const BASE_URL = 'https://quiz-zeta-seven.vercel.app/api'
 
 export default function Home() {
   const router = useRouter()
